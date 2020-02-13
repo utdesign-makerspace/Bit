146163351bb50e870ada50ea04964bb7007b2fbf
BigWhiteCrayon <jordanryoungberg@gmail.com>
2020-02-12 20:53:21 -0600

    preliminary Minecraft Auth

--- commands/MinecraftAuthCommand.js
+++ commands/MinecraftAuthCommand.js
@@ -1,24 +1,44 @@
-const fetch = require("node-fetch");
+const { Command } = require('discord-akairo');
 
-function oAuthToUUID(token) {
-   return fetch('https://mc-oauth.net/api/api?token', {
-      method: "GET",
-      headers: {
-         "token": token
+class MinecraftAuthCommand extends Command {
+   constructor() {
+       super('Minecraft Account Authorization', { //Official dicord command name TBD
+          aliases: ['MAA','Minecraft Account Authorization'] 
+       });
+   }
+
+   async exec(message) {
+      const userMsg = message.content;
+      let userInfo = {
+        uuid = null,
+        username = null
       }
-   }).then(response => {
-      return response.json();
-   }).then(json => {
-      return json.status === 'success' ? json.uuid : 'fail';
-   });
+
+      let token = message.content.match(/?(*)/);
+      token = token[1];
+      console.log(userMsg);
+       
+      message.reply('A minecraft account is trying to be authorized for '+message.author.username); // Add in the user
+      console.log("\nA minecraft account is trying to be authorized");
+
+      fetch('https://mc-oauth.net/api/api?token', {
+         method: "GET",
+         headers: {
+            "token": token
+         }
+      }).then(response => {
+         return response.json();
+      }).then(json => {
+         if(json.status === 'success'){
+            userInfo.uuid = json.uuid; 
+            userInfo.username = json.username;
+            message.reply('Thank you ' + userInfo.username);
+         }
+         else{
+            message.reply('error');
+         }
+      });
+   }
 }
 
-oAuthToUUID(uuid => {
-   if (uuid !== 'fail') {
-      // Handle successful login
-      console.log("You successfully authenticated your minecraft account!")
-   } else {
-      // Handle invalid token
-      console.log("You failed to authenticate your minecraft account... Please try again")
-   }
-});
+module.exports = MinecraftAuthCommand;
